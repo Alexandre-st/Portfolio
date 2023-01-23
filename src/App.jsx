d1ddd6823605b743a91b8fbf5d390f0950ea6cb2
Alexandre-st <alexandre.saintprix@gmail.com>
2023-01-23 20:24:57 +0100

    Add a base for header without a lot of styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./layouts/Header";
+import Homepage from "./pages/Homepage";
+
+
 const App = () => {
 
   return (
-    <h1>Hello World</h1>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
   )
 };
 
