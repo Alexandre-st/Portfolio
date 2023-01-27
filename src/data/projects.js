7e4f6b1a90601a4829df309a92e9952d61d4d571
Alexandre-st <alexandre.saintprix@gmail.com>
2023-01-27 05:57:16 +0100

    Add the base for the data contents

---
+++ src/data/projects.js
@@ -0,0 +1,14 @@
+export const projects = [
+  {
+    image: '../assets/images/projects/designo-app-design.png',
+    title: 'Designo',
+    infos: '',
+    technos: ['', ''],
+    link: '',
+    desc: '',
+    preview1: '',
+    preview2: '',
+    prevProject: '',
+    nextProject: '',
+  },
+]
