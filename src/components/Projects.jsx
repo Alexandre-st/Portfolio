ae7fc1f63b0a0244e449a5eda88d6dd5d58acc82
Alexandre-st <alexandre.saintprix@gmail.com>
2023-01-28 14:11:27 +0100

    Add another project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,19 +1,18 @@
 import { projects } from '../data/projects';
 
 const Projects = () => {
-  // console.log(projects);
   return (
     <section className="projects">
       {projects.map(project => {
         return (
           <div className={project.styleName} key={project.id}>
-            <div className="projects-background" style={{ backgroundColor: project.color }}>
-            <p>{projects.color}</p>
+            <div className="projects-background" style={{ background: project.color }}>
               <img src={project.image} alt={project.title} />
             </div>
             <div className="projects-content">
-              <h2>{project.title}</h2>
-              <p>{project.infos}</p>
+              <div className="border"></div>
+              <h2 className='projects-content-title big-title'>{project.title}</h2>
+              <p className='text'>{project.infos}</p>
             </div>
           </div>
         )
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,20 +1,11 @@
-// import bg from "../assets/images/projects/designo-app-design.png";
 import Projects from "../components/Projects";
 
 const Portfolio = () => {
   return (
-    <div className="container">
-      <h1>Portfolio</h1>
-      <main>
-        <Projects />
-      </main>
-      {/* <div className='images'>
-        <div className='images-color'>
-          <img src={bg} alt="BG" />
-        </div>
-      </div> */}
-    </div>
+    <main className="container">
+      <Projects />
+    </main>
   );
-}
+};
  
 export default Portfolio;
