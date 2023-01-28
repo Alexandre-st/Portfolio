99e1f5bd19e4dfd78e60db67c0c8a88f04aeca4f
Alexandre-st <alexandre.saintprix@gmail.com>
2023-01-28 17:56:22 +0100

    Add the tablet contact design but need to work again

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,17 +1,40 @@
+import { Link } from "react-router-dom";
+import github from "../assets/github-mark.svg";
+import linkedIn from "../assets/iconmonstr-linkedin-3.svg";
+import twitter from "../assets/twitter.svg";
 import Form from "../components/Form";
 
 const Contact = () => {
   return (
     <main className="container">
       <section className="contact-hero">
-        <h1 className="contact-hero-title">Premier contact</h1>
-        <p className="contact-hero-about">
-          J'aimerai beaucoup savoir sur quoi vous travaillez actuellement et comment je pourrai aider. Je suis actuellement à la recherche d'un nouveau poste et je suis ouvert à une multitude d'opportunité.
-          Ma préférence serait de trouver une place dans une entreprise à Paris, mais je suis ouvert aux opportunités qui ne correspondant pas à la description précédente.
-          Je suis un amoureux du travail, une personne positive qui approchera chaque tâches avec un sens du détail et une attention particulière pour la réalisation de l'objectif. 
-        </p>
+        <h1 className="contact-hero-title big-title">Premier contact</h1>
+        <div className="contact-hero-content">
+          <p className="contact-hero-about text">
+            J'aimerai beaucoup savoir sur quoi vous travaillez actuellement et
+            comment je pourrai vous aider. Je suis actuellement à la recherche
+            d'un nouveau poste et je suis ouvert à une multitude d'opportunité.
+            Ma préférence serait de trouver une place dans une entreprise à
+            Paris, mais je suis ouvert aux opportunités qui ne correspondant pas
+            à la description précédente. Je suis un amoureux du travail, une
+            personne positive qui approchera chaque tâches avec un sens du
+            détail et une attention particulière pour la réalisation de
+            l'objectif.
+          </p>
+          <div className="contact-hero-link">
+            <Link to="https://github.com/Alexandre-st">
+              <img src={github} alt="Lien pour accéder à mon Github" />
+            </Link>
+            <Link to="https://twitter.com/a_saintprix">
+              <img src={twitter} alt="Lien pour accéder à mon Twitter" />
+            </Link>
+            <Link to="https://www.linkedin.com/in/alexandre-saint-prix">
+              <img src={linkedIn} alt="Lien pour accéder à mon LinkedIn" />
+            </Link>
+          </div>
+        </div>
       </section>
-     <Form />
+      <Form />
     </main>
   );
 };
