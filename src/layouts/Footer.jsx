fae8ce6b973c3bfcf38f47052f01b4353f264870
Alexandre-st <alexandre.saintprix@gmail.com>
2023-02-06 22:13:19 +0100

    Update the footer design

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import github from "../assets/icons/github-mark.svg";
+import linkedIn from "../assets/icons/iconmonstr-linkedin-3.svg";
+import twitter from "../assets/icons/twitter.svg";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container container">
-        <h4>Footer</h4>
-
         <ul className="footer-content-list">
           <li className="nav-link">
             <Link to="/">Home</Link>
@@ -19,9 +20,18 @@ const Footer = () => {
         </ul>
 
         <div className="footer-social">
-          <a href="https://github.com/Alexandre-st" target="_blank">Github</a>
-          <a href="https://twitter.com/a_saintprix" target='_blank'>Twitter</a>
-          <a href="https://www.linkedin.com/in/alexandre-saint-prix/" target='_blank'>LinkedIn</a>
+          <a href="https://github.com/Alexandre-st" target="_blank">
+            <img src={github} alt="Lien pour accéder à mon Github" />
+          </a>
+          <a href="https://twitter.com/a_saintprix" target="_blank">
+            <img src={twitter} alt="Lien pour accéder à mon Twitter" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/alexandre-saint-prix/"
+            target="_blank"
+          >
+            <img src={linkedIn} alt="Lien pour accéder à mon LinkedIn" />
+          </a>
         </div>
       </div>
     </footer>
